a6f6edb051aa96a3cf8daa6b1f60dcd93ce275b8
Eugene Sim <eugenesgj@gmail.com>
2019-11-06 19:55:18 +0800

    import init

---
+++ view/app/src/routes/ims/inventory/import/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ims_inventory_massimport extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>import inventory</p>
+      </div>
+    );
+  }
+}
+
+export default ims_inventory_massimport;
