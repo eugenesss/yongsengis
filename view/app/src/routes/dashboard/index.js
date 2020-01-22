8a249671fff184cb93ecfce4226623cfcd619353
Eugene Sim <eugenesgj@gmail.com>
2020-01-22 18:30:24 +0800

    stockcount ui template

--- view/app/src/routes/dashboard/index.js
+++ view/app/src/routes/dashboard/index.js
@@ -7,6 +7,7 @@ import BgCard from "Components/BgCard";
 
 // widgets
 import TodoList from "Components/Widgets/TodoList";
+// import StockCount from "Components/Widgets/StockCount";
 
 class Homebase extends Component {
   render() {
@@ -16,9 +17,7 @@ class Homebase extends Component {
           <title>YSIS | Dashboard</title>
         </Helmet>
         <div className="row">
-          <div className="col-md-7">
-            <BgCard>zero stock count</BgCard>
-          </div>
+          <div className="col-md-7">{/* <StockCount /> */}</div>
           <div className="col-md-5">
             <TodoList />
           </div>
